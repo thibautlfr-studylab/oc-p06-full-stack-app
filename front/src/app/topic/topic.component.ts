c61e87b772dfd8e9ab4c817c78ea550670ca6cde
romainsessa <romainsessa@gmail.com>
2025-11-24 22:08:45 +0100

    upgrade to angular 20

--- front/src/app/topic/topic.component.ts
+++ front/src/app/topic/topic.component.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-topic',
-  templateUrl: './topic.component.html',
-  styleUrls: ['./topic.component.css']
+    selector: 'app-topic',
+    templateUrl: './topic.component.html',
+    styleUrls: ['./topic.component.css'],
+    standalone: false
 })
 export class TopicComponent {
 
